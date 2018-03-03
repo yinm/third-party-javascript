ac29492d601995759c5b0e9b38ccbeeaf7773ee4
yinm <yinm.cminor9@gmail.com>
2018-03-03 14:28:40 +0900

    Check multiple rendering

--- examples/templates/03/rendering-multiple/widget.js
+++ examples/templates/03/rendering-multiple/widget.js
@@ -1,22 +1,21 @@
 if (!window.Stork) {
-    Stork = {};
+  Stork = {};
 }
 
 (function(Stork, window, undefined) {
+  if (typeof Stork.widgetCount === 'undefined') {
+    Stork.widgetCount = 0;
+  }
 
-    if (typeof Stork.widgetCount === 'undefined') {
-        Stork.widgetCount = 0;
-    }
+  function append(html) {
+    $('[data-stork-widget]').each(function(i, elem) {
+      if (Stork.widgetCount === i) {
+        $(elem).before(html);
+        Stork.widgetCount++;
+        return;
+      }
+    });
+  }
 
-    function append(html) {
-        $('[data-stork-widget]').each(function(i, elem) {
-            if (Stork.widgetCount === i) {
-                $(elem).before(html);
-                Stork.widgetCount++;
-                return;
-            }
-        });
-    }
-
-    append('<div>Widget HTML</div>');
+  append('<div>Widget HTML</div>');
 })(Stork, window);
