b43d0cf4ad2ab7db6f4dc8f6d5dd5e56d5db0d9f
yinm <yinm.cminor9@gmail.com>
2018-03-03 13:04:53 +0900

    Check capturing data-*

--- examples/templates/02/passing-args/data-attr.js
+++ examples/templates/02/passing-args/data-attr.js
@@ -1,17 +1,22 @@
 (function() {
-    function getProductId() {
-        var scripts = document.getElementsByTagName('script'),
-            id;
+  function getProductId() {
+    var
+      scripts = document.getElementsByTagName('script'),
+      id;
 
-        for (var i = 0; i < scripts.length; i++) {
-            id = scripts[i].getAttribute('data-stork-productId');
-            if (id)
-            return id;
-        }
-        return null;
+    for (var i = 0; i < scripts.length; i++) {
+      id = scripts[i].getAttribute('data-stork-productId');
+      if (id) {
+        return id;
+      }
     }
-    var params = {
-        'productId': getProductId()
-    };
-    document.getElementById('data-attr-out').innerHTML = JSON.stringify(params);
+
+    return null;
+  }
+
+  var params = {
+    'productId': getProductId();
+  };
+
+  document.getElementById('data-attr-out').innerHTML = JSON.stringify(params);
 })();
