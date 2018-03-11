b31e15e2a40a10b156b83393fbfbfbeeb5c64412
yinm <yinm.cminor9@gmail.com>
2018-03-11 10:01:12 +0900

    Check initialization of SDK

--- examples/templates/08/web-service-api/sdk.js
+++ examples/templates/08/web-service-api/sdk.js
@@ -1,16 +1,17 @@
 (function(window, undefined) {
-    var Stork = {};
+  var Stork = {};
 
-    if (window.Stork)
-        return;
+  if (window.Stork) {
+    return;
+  }
 
-    Stork.init = function(callback) {
-        Stork.script("{{ service_url_for('example', chapter='08', name='web-service-api', file='lib.js') }}", function () {
-            Stork._initializeLibrary(callback);
-        });
-    };
+  Stork.init = function(callback) {
+    Stork.script("{{ service_url_for('example', chapter='08', name='web-service-api', file='lib.js') }}", function() {
+      Stork._initializeLibrary(callback);
+    });
+  };
 
-    window.Stork = Stork;
+  window.Stork = Stork;
 })(this);
 
 // Embed the 'script' microlib into our initial script file for loading dependencies.
